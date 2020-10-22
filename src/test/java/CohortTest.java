0f34b204cdd9512e61d71fe735e9688805824469
Chelsea <chelsea.e.rolland@gmail.com>
2020-10-22 17:31:17 -0500

    completed the cohort test

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -4,44 +4,61 @@ import org.junit.Test;
 
 public class CohortTest {
 
-    public Cohort moon;
-    public Cohort slate;
+    public Cohort moonEmpty;
+    public Cohort pulseOne;
+    public Cohort slateMany;
     public Student chelsea;
     public Student alyis;
     public Student stratus;
 
     @Before
     public void setup() {
-        //Empty
-        slate = new Cohort();
+        //Instantiate
+        moonEmpty = new Cohort();
+        pulseOne = new Cohort();
+        slateMany = new Cohort();
 
-        //Multiple Students
-        moon = new Cohort();
+        //Students
         chelsea = new Student("Chelsea", 1L);
         alyis = new Student("Alyis", 2L);
         stratus = new Student("Stratus", 3L);
 
-        moon.addStudent(chelsea);
-        moon.addStudent(alyis);
+        //Adding Grades
+        chelsea.addGrade(100);
+        alyis.addGrade(80);
+        alyis.addGrade(90);
+        alyis.addGrade(70);
+
+        //adding to cohort
+        pulseOne.addStudent(chelsea);
+
+        slateMany.addStudent(chelsea);
+        slateMany.addStudent(alyis);
     }
 
     @Test
     public void testAddStudent(){
-        moon.addStudent(stratus);
-        assertEquals(stratus, moon.getStudents().get(3));
+        slateMany.addStudent(stratus);
+        assertEquals("Stratus", slateMany.getStudents().get(2).getName());
+        slateMany.addStudent(new Student("Lofi Misses", 5L));
+        assertEquals("Lofi Misses", slateMany.getStudents().get(3).getName());
+        assertEquals(4, slateMany.getStudents().size());
     }
 
     @Test
     public void testCurrentStudent(){
+        assertEquals(0, moonEmpty.getStudents().size());
+        assertEquals(1, pulseOne.getStudents().size());
+
+        assertEquals(1, slateMany.getStudents().get(0).getId());
+        assertEquals(2, slateMany.getStudents().get(1).getId());
     }
 
     @Test
     public void testGetAverage(){
-        chelsea.addGrade(100);
-        chelsea.addGrade(80);
-        assertEquals(90.0, chelsea.getGradeAverage(), 0);
-        chelsea.addGrade(90);
-        assertEquals(93.33, chelsea.getGradeAverage(), 0.5);
+        assertEquals(Double.NaN, moonEmpty.getCohortAverage(), 0);
+        assertEquals(100.0, pulseOne.getCohortAverage(), 0);
+        assertEquals(90.0, slateMany.getCohortAverage(), 0);
     }
 
 }
